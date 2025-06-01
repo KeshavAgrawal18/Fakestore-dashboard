c53e6a3dfd91f46849fbb67146736a27a67fb060
keshav Agrawal <keshavagrawal178@gmail.com>
2025-06-02 03:15:53 +0530

    fix: remove duplicate @babel/preset-env entry to resolve syntax error

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-env", // Transpile modern JS
-    "@babel/preset-react", // Transpile JSX
-  ],
-};
